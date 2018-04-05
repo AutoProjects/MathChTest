1cafff4b6e7c29497ee5dd086e75e4dce153ec81
Jenkins_room8 <jenkins@room8studio.com>
2018-04-05 16:12:53 +0300

    check script 2

---
+++ Assets/Editor/PerformBuild copy 2.cs
@@ -0,0 +1,86 @@
+using UnityEditor;
+using System.IO;
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+[InitializeOnLoad]
+public class PreloadSigningAlias
+{
+    static PreloadSigningAlias()
+    {
+        PlayerSettings.Android.keystorePass = "111111";
+        PlayerSettings.Android.keyaliasName = "math.alias";
+        PlayerSettings.Android.keyaliasPass = "111111";
+    }
+}
+
+class PerformBuild
+{
+    static string[] GetBuildScenes()
+	   {
+		     List<string> names = new List<string>();
+
+        foreach(EditorBuildSettingsScene e in EditorBuildSettings.scenes)
+        {
+            if(e==null)
+                continue;
+
+            if(e.enabled)
+                names.Add(e.path);
+        }
+        return names.ToArray();
+    }
+
+    static string GetBuildPath()
+    {
+        return "/Users/jenkins/Documents/Math_build/iPhone";
+    }
+
+    [UnityEditor.MenuItem("CUSTOM/Test Command Line Build Step")]
+    static void CommandLineBuild ()
+    {
+        PlayerSettings.iOS.buildNumber = "1";
+        Debug.Log("Command line build\n------------------\n------------------");
+
+        string[] scenes = GetBuildScenes();
+        string path = GetBuildPath();
+        if(scenes == null || scenes.Length==0 || path == null)
+            return;
+
+        Debug.Log(string.Format("Path: \"{0}\"", path));
+        for(int i=0; i<scenes.Length; ++i)
+        {
+            Debug.Log(string.Format("Scene[{0}]: \"{1}\"", i, scenes[i]));
+        }
+
+        Debug.Log("Starting Build!");
+        BuildPipeline.BuildPlayer(scenes, path, BuildTarget.iOS, BuildOptions.None);
+    }
+
+    static string GetBuildPathAndroid()
+    {
+        return "/Users/jenkins/Documents/Math_build/android";
+    }
+
+    [UnityEditor.MenuItem("CUSTOM/Test Command Line Build Step Android")]
+    static void CommandLineBuildAndroid ()
+    {
+        PlayerSettings.bundleVersion = "0.3.0";
+        Debug.Log("Command line build android version\n------------------\n------------------");
+
+        string[] scenes = GetBuildScenes();
+        string path = GetBuildPathAndroid();
+        if(scenes == null || scenes.Length==0 || path == null)
+            return;
+
+        Debug.Log(string.Format("Path: \"{0}\"", path));
+        for(int i=0; i<scenes.Length; ++i)
+        {
+            Debug.Log(string.Format("Scene[{0}]: \"{1}\"", i, scenes[i]));
+        }
+
+        Debug.Log("Starting Android Build!");
+        BuildPipeline.BuildPlayer(scenes, path, BuildTarget.Android, BuildOptions.None);
+    }
+}
